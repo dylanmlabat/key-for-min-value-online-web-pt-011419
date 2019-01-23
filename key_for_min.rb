a81c0306cd8533c76d234a4078d009961f96f4a2
Dylan Michael Labat <dml252@live.com>
2019-01-23 03:23:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,5 @@ def key_for_min_value(name_hash)
   return nil if name_hash.empty?
   min_key, min_value = name_hash.first
   name_hash.each do |key, value|
-    if min_value < value
+    if value < min_value
 end
